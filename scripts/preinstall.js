714ec40e3726b1f7bf52813d4cb5c425108bd63f
Li_Peter <505175463@qq.com>
2023-07-31 19:25:56 +0800

    feat:project init && config

---
+++ scripts/preinstall.js
@@ -0,0 +1,8 @@
+// npm在install的时候会触发preinstall生命周期钩子从而执行这里的文件.
+if (!/pnpm/.test(process.env.npm_execpath || '')) {
+  console.warn(
+    `\u001b[33mThis repository must using pnpm as the package manager ` +
+    ` for scripts to work properly.\u001b[39m\n`,
+  )
+  process.exit(1)
+}
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+console.log("测试");
+</script>
+
+<template>
+  <h1>APP index</h1>
+</template>
+
+<style scoped></style>
