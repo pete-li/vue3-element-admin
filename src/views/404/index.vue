6e24e44850eebf9542fc56f1011ab08ba3f339e5
Li_Peter <505175463@qq.com>
2023-08-26 23:22:31 +0800

    feat: home && 404 page

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,10 +1,36 @@
 <template>
-  <h1>404页面</h1>
+  <div class="box">
+    <img
+      class="error-404"
+      src="../../assets/images/error_images/404.png"
+      alt="图片丢失了"
+      title="点击返回首页"
+      @click="router.push('./')"
+    />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
 
-<style></style>
+const router = useRouter()
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .error-404 {
+    width: 500px;
+    margin: 0 100px 50px 0;
+    cursor: pointer;
+  }
+}
+</style>
 
 <script lang="ts">
 export default {
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,73 @@
 <template>
-  <h1>home页面</h1>
+  <div class="admin-home">
+    <el-card class="user-panel">
+      <div style="display: flex">
+        <img class="user-avatar" :src="userStore.avatar" alt="图片丢失了" />
+        <div class="user-info">
+          <p class="hello-name">{{ getTime() }}好~ {{ userStore.username }}</p>
+          <p class="sub-title">vue3-element-admin</p>
+        </div>
+      </div>
+    </el-card>
+    <div class="bottom">
+      <SvgIcon name="welcome" width="600px" height="300px"></SvgIcon>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import useUserStore from '@/store/modules/user.ts'
+import SvgIcon from '@/components/SvgIcon/index.vue'
+import { getTime } from '@/utils/getTime.ts'
 
-<style></style>
+const userStore = useUserStore()
+</script>
+
+<style lang="scss" scoped>
+.admin-home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  .user-panel {
+    height: 200px;
+    margin: 16px 16px 0 16px;
+
+    .user-avatar {
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      margin-bottom: 30px;
+    }
+
+    .user-info {
+      width: 100%;
+      height: 100%;
+
+      .hello-name {
+        font-size: 35px;
+        font-weight: 900;
+        margin: 16px 0 16px 16px;
+      }
+
+      .sub-title {
+        font-style: italic;
+        color: #ccc;
+        font-weight: 700;
+        margin-left: 16px;
+      }
+    }
+  }
+
+  .bottom {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
 
 <script lang="ts">
 export default {
